1dcd9d239fd13b10500fa1641dcbfc29d777f706
eric612 <eric612kimo@yahoo.com.tw>
2021-04-23 15:22:52 +0800

    Update yolo_loss.py

--- models/voc/yolo_loss.py
+++ models/voc/yolo_loss.py
@@ -216,7 +216,7 @@ class YOLOLoss(nn.Module):
             loss = self.weighted_mse_loss(output , target , weights)
             iou_target = torch.ones_like(iou_losses)
             #iou_loss= torch.sum(iou_target-iou_losses)
-            iou_loss = torch.Tensor([0]) 
+            iou_loss = torch.Tensor([0]).to(device) 
             if iou_losses.size(0)>0:
                 iou_loss = self.weighted_mse_loss(iou_losses,iou_target,iou_weights)/iou_losses.numel()
             #iou_loss = self.mse_loss(iou_losses,iou_target)/iou_losses.numel()
