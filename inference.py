8321d5681bb2dd4ebc7840cac26b0edd50ef1c7c
Eric Liu <eric612kimo@yahoo.com.tw>
2021-07-06 18:23:19 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -95,7 +95,7 @@ def main():
 def inference_image(model, original_image,device):
     # Transforms
     transform_test = transforms.Compose([
-        transforms.Resize(size=(416,416), interpolation=2),
+        transforms.Resize(size=(352,352), interpolation=2),
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
